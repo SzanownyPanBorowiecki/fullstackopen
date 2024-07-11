20a4c1924b1bd4fa9b8b08eb86491df961f97914
Pawel <borowieckipawel@yahoo.co.uk>
2024-07-11 16:41:39 +0200

    complete part 6d

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
