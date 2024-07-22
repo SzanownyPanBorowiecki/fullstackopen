f179f8c545606e6a6dc743908f64877f31827440
Pawel <borowieckipawel@yahoo.co.uk>
2024-07-22 14:18:10 +0200

    finish ex 8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -90,9 +90,18 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
     dummy: Int
   }
 `
@@ -101,6 +110,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
     dummy: () => 0
   }
 }
