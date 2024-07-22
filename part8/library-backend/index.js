571523e7cb4831058552e3450a1f7e00cf4fedb3
Pawel <borowieckipawel@yahoo.co.uk>
2024-07-22 14:59:23 +0200

    finish ex 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -121,6 +121,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -162,6 +167,15 @@ const resolvers = {
       books = books.concat(book)
 
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) return null
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
